581b0429f2e102261ff6855c83473ba162e265b2
Daniel Tingelöf <daniel.tingelof@gmail.com>
2024-09-28 20:36:42 +0200

    Setup convenient keybinds for flycheck

--- post-init.el
+++ post-init.el
@@ -346,6 +346,36 @@ Unlike `comment-dwim', this always comments whole lines."
 (use-package flycheck
   :ensure t
   :init (global-flycheck-mode))
+b
+(use-package hydra
+  :defer 2
+  :bind ("C-c f" . hydra-flycheck/body))
+
+(defhydra hydra-flycheck (:color blue)
+  "
+  ^
+  ^Flycheck^          ^Errors^            ^Checker^
+  ^────────^──────────^──────^────────────^───────^─────
+  _q_ quit            _>_ previous        _?_ describe
+  _M_ manual          _<_ next            _d_ disable
+  _v_ verify setup    _f_ check           _m_ mode
+  ^^                  _l_ list            _s_ select
+  ^^                  ^^                  ^^
+  "
+  ("q" nil)
+  (">" flycheck-previous-error :color pink)
+  ("<" flycheck-next-error :color pink)
+  ("?" flycheck-describe-checker)
+  ("M" flycheck-manual)
+  ("d" flycheck-disable-checker)
+  ("f" flycheck-buffer)
+  ("l" flycheck-list-errors)
+  ("m" flycheck-mode)
+  ("s" flycheck-select-checker)
+  ("v" flycheck-verify-setup))
+
+(global-set-key (kbd "M-n") 'flycheck-next-error)
+(global-set-key (kbd "M-p") 'flycheck-previous-error)
 
 ;; Use both pylint and flake8 for Python
 ;; Generate pylint rc: pylint --generate-rcfile > ~/.pylintrc
