24e7d5ab66129ac6458640e8e1f848ed62060cfb
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2024-10-16 20:19:19 +0200

    Theme tweaks

--- custom-file.el
+++ custom-file.el
@@ -6,7 +6,7 @@
  '(before-save-hook '(delete-trailing-whitespace))
  '(compilation-scroll-output t)
  '(custom-safe-themes
-    '("c4847a17d607ae479425b4180f912a5f8b15aaf6602dcdbdf80b45f4f7b5c86f" "ab0e5658892d375ac996b68a4b32721e327f10246d62e1ba59e539477d167831" "283f9c7e709629a69635256b38d163c60025c7397ce0ab2012b5aa447688f052" "3f5397595ce0cab1951ef74cd9f76c0a34e9c6c7183a9c93eca11dccb7a39093" "b9f44212b4be6f0466811c5d8a297dda3c40dbf4c4cfd97c1686fceb2043b617" "6e13ff2c27cf87f095db987bf30beca8697814b90cd837ef4edca18bdd381901" "2119133daf4639a0151886f2a13ca32284ed7388a1b7d2401e8886d376a4b0a8"))
+    '("968f921a942756576efadb721bbf23f5d983b4186251920a1d5762a49a2d00e6" "c4847a17d607ae479425b4180f912a5f8b15aaf6602dcdbdf80b45f4f7b5c86f" "ab0e5658892d375ac996b68a4b32721e327f10246d62e1ba59e539477d167831" "283f9c7e709629a69635256b38d163c60025c7397ce0ab2012b5aa447688f052" "3f5397595ce0cab1951ef74cd9f76c0a34e9c6c7183a9c93eca11dccb7a39093" "b9f44212b4be6f0466811c5d8a297dda3c40dbf4c4cfd97c1686fceb2043b617" "6e13ff2c27cf87f095db987bf30beca8697814b90cd837ef4edca18bdd381901" "2119133daf4639a0151886f2a13ca32284ed7388a1b7d2401e8886d376a4b0a8"))
  '(eglot-connect-timeout nil t)
  '(gnutls-algorithm-priority "normal:-vers-tls1.3")
  '(ignored-local-variable-values
@@ -31,4 +31,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#191919" :foreground "#ccd1db" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight ultra-light :height 140 :width normal :foundry "JB" :family "JetBrains Mono")))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#191919" :foreground "#ccd1db" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight ultra-light :height 140 :width normal :foundry "JB" :family "JetBrains Mono"))))
+ '(isearch-fail ((t (:background "#d46a76"))))
+ '(mode-line ((t (:background "#252525" :foreground "#e1e4e8"))))
+ '(mode-line-active ((t (:background "#252525" :foreground "#e1e4e8"))))
+ '(mode-line-buffer-id ((t (:foreground "#729fdd"))))
+ '(mode-line-inactive ((t (:background "gray12" :foreground "#e1e4e8"))))
+ '(region ((t (:extend t :background "gray20" :foreground "white smoke"))))
+ '(tabbar-button ((t (:background "#252525" :foreground "#252525"))))
+ '(tabbar-default ((t (:background "#252525"))))
+ '(tabbar-highlight ((t (:background "#252525" :foreground "#ffa657"))))
+ '(tabbar-modified ((t (:background "#252525" :foreground "#729fdd"))))
+ '(tabbar-selected ((t (:background "#252525" :foreground "#e1e4e8"))))
+ '(tabbar-selected-modified ((t (:background "#252525" :foreground "#d46a76"))))
+ '(tabbar-separator ((t (:foreground "#729fdd"))))
+ '(tabbar-unselected ((t (:background "#252525" :foreground "#729fdd")))))
